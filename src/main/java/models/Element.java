574a11ef773d8534a5b115ef956942b26ce6ca74
ZEvans1 <zte.zachary@gmail.com>
2018-01-26 11:00:52 -0800

    Initial commit.

---
+++ src/main/java/models/Element.java
@@ -0,0 +1,18 @@
+package models;
+
+public class Element {
+
+    private String name;
+    private String symbol;
+    private int atomicNumber;
+    private int atomicMass;
+    private int groupNumber;
+    private int periodNumber;
+    private int id;
+    private int groupId;
+    private int periodId;
+
+    public Element(String name, String symbol, int atomicNumber, int atomicMass, int groupNumber, int periodNumber, int groupId, int periodId) {
+
+    }
+}
